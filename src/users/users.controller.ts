7173ed3d4470e04ece05efb4faf18a8ce1b637f9
SofiiaKozlyk <ipz222_kso@student.ztu.edu.ua>
2024-11-24 16:12:29 +0200

    Added methods for finding user by id or username

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { BadRequestException, Body, Controller, Get, NotFoundException, Post, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
+import { plainToInstance } from 'class-transformer';
+import { User } from './user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly appService: UsersService) {}
+    constructor(private readonly usersService: UsersService) {}
 
     @Get()
     @ApiOperation({ summary: 'Получение списка пользователей' })
@@ -12,10 +15,22 @@ export class UsersController {
     @ApiQuery({ name: 'username', required: false, description: 'Имя пользователя' })
     @ApiResponse({ status: 200, description: 'Успешное получение списка пользователей' })
     @ApiResponse({ status: 404, description: 'Пользователи не найдены' })
-    async getAllUsers(
+    async getUser(
         @Query('id') id?: number,
         @Query('username') username?: string,
     ){
+        if (!id && !username) {
+            throw new NotFoundException('ID или username должны быть указаны');
+        }
 
+        const user = id ?
+            await this.usersService.findById(id) :
+            await this.usersService.findByUsername(username);
+
+        if (!user) {
+            throw new NotFoundException('Пользователь не найден');
+        }
+
+        return plainToInstance(User, user, { excludeExtraneousValues: true });
     }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+      ) {}
+
+    async findByUsername(username: string): Promise<User | undefined> {
+        return this.usersRepository.findOne({ where: { username } });
+    }
+
+    async findById(id: number): Promise<User | undefined> {
+        return this.usersRepository.findOne({ where: { id } });
+    }
+}
